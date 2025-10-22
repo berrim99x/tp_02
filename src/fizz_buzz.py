c9c7ddd1f073de1857df03b50d73c1797ebc1f1b
Abdelhakim <hakimhakim20046@gmail.com>
2025-10-22 14:51:40 +0200

    fizz_buzz(1) == '1'

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,2 +1,4 @@
 def fizz_buzz(number: int) -> str:
+    if number == 1 :
+        return '1'
     return 'fizzbuzz'
--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -3,3 +3,5 @@ from src.fizz_buzz import fizz_buzz
 
 def test_0_returns_fizz_buzz():
     assert fizz_buzz(0) == 'fizzbuzz'
+def test_1_returns_1():
+    assert fizz_buzz(1) == '1'
