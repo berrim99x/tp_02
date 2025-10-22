1e684e4717a3f3e402dc4f778430729f2b8d3106
Abdelhakim <hakimhakim20046@gmail.com>
2025-10-22 15:12:35 +0200

    fizz_buzz(10)='buzz'

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -3,7 +3,7 @@ def fizz_buzz(number: int) -> str:
         return 'fizzbuzz'
     if number % 3 == 0:
         return 'fizz'
-    if number == 5 :
+    if number == 5 or number == 10 :
         return 'buzz'
     return str(number)
 
