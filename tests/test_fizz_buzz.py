c1bd58c8c5371d3abca1fb190a10ba1b316a75ae
Abdelhakim <hakimhakim20046@gmail.com>
2025-10-22 15:35:29 +0200

    many tests

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.fizz_buzz import fizz_buzz
 
-@pytest.mark.parametrize('number,result_expected', [(0,'fizzbuzz'),(1,'1'),(2,'2'),(3,'fizz'),(4,'4'),(5,'buzz'),(6,'fizz'),(7,'7'),(8,'8'),(9,'fizz'),(10,'buzz'),(11,'11'),(12,'fizz'),(13,'13'),(14,'14'),(15,'fizzbuzz'),(16,'16'),(17,'17'),(18,'fizz'),(19,'19'),(20,'buzz'),(21,'fizz'),(22,'22'),(23,'23'),(24,'fizz'),(25,'buzz'),(26,'26'),(27,'fizz'),(28,'28'),(29,'29'),(30,'fizzbuzz')
+@pytest.mark.parametrize('number,result_expected', [(0,'fizzbuzz'),(1,'1'),(2,'2'),(3,'fizz'),(4,'4'),(5,'buzz'),(6,'fizz'),(7,'7'),(8,'8'),(9,'fizz'),(10,'buzz'),(11,'11'),(12,'fizz'),(13,'13'),(14,'14'),(15,'fizzbuzz'),(16,'16'),(17,'17'),(18,'fizz'),(19,'19'),(20,'buzz'),(21,'fizz'),(22,'22'),(23,'23'),(24,'fizz'),(25,'buzz'),(26,'26'),(27,'fizz'),(28,'28'),(29,'29'),(30,'fizzbuzz'),(35,'buzz'),(41,'41'),(50,'buzz'),(51,'fizz'),(60,'fizzbuzz'),(300,'fizzbuzz'),(715,'buzz'),(3315,'fizzbuzz'),(11111,'11111')
 ])
 def test_number_returns_result_expected(number, result_expected):
     actual_result = fizz_buzz(number)
