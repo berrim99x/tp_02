332b76190de4d3a75ff993ac55cef71931039ca4
Abdelhakim <hakimhakim20046@gmail.com>
2025-10-22 15:24:48 +0200

    refactoring

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -3,7 +3,7 @@ def fizz_buzz(number: int) -> str:
         return 'fizzbuzz'
     if number % 3 == 0:
         return 'fizz'
-    if number == 5 or number == 10 or number == 20 :
+    if number % 5 == 0 :
         return 'buzz'
     return str(number)
 
